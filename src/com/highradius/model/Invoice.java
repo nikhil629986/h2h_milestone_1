55e2cbd3d880b5e6be4753456a6cd54621f0d63a
NIKHIL <nikhil629986@gmail.com>
2023-06-06 10:55:08 +0530

    invoices update

--- src/com/highradius/model/Invoice.java
+++ src/com/highradius/model/Invoice.java
@@ -1,17 +1,108 @@
 package com.highradius.model;
 
 public class Invoice {
-    private int id;
+    private int SI_No;
+    private int CustOrderID;
+    private int SalesOrg;
+    private String DistributionChannel;
+    private int CompanyCode;
+    private String OrderCreationDate;
+    private String OrderCurrency;
+    private int CustNo;
+    private double AmountInUSD;
+    private double OrderAmount;
 
-    public Invoice(int id) {
-        this.id = id;
+    public Invoice(int SI_No, int custOrderID, int salesOrg, String distributionChannel,
+                   int companyCode, String orderCreationDate, String orderCurrency, int custNo,
+                   double amountInUSD, double orderAmount) {
+        this.SI_No = SI_No;
+        CustOrderID = custOrderID;
+        SalesOrg = salesOrg;
+        DistributionChannel = distributionChannel;
+        CompanyCode = companyCode;
+        OrderCreationDate = orderCreationDate;
+        OrderCurrency = orderCurrency;
+        CustNo = custNo;
+        AmountInUSD = amountInUSD;
+        OrderAmount = orderAmount;
     }
 
-    public int getId() {
-        return id;
+   
+    public int getSI_No() {
+        return SI_No;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setSI_No(int SI_No) {
+        this.SI_No = SI_No;
+    }
+
+    public int getCustOrderID() {
+        return CustOrderID;
+    }
+    public void setCustOrderID(int custOrderID) {
+        CustOrderID = custOrderID;
+    }
+
+    public int getSalesOrg() {
+        return SalesOrg;
+    }
+    public void setSalesOrg(int salesOrg) {
+        SalesOrg = salesOrg;
+    }
+
+    public String getDistributionChannel() {
+        return DistributionChannel;
+    }
+
+    public void setDistributionChannel(String distributionChannel) {
+        DistributionChannel = distributionChannel;
+    }
+    public int getCompanyCode() {
+        return CompanyCode;
+    }
+    public void setCompanyCode(int companyCode) {
+        CompanyCode = companyCode;
+    }
+
+    public String getOrderCreationDate() {
+        return OrderCreationDate;
+    }
+
+    public void setOrderCreationDate(String orderCreationDate) {
+        OrderCreationDate = orderCreationDate;
+    }
+
+    public String getOrderCurrency() {
+        return OrderCurrency;
+    }
+
+    public void setOrderCurrency(String orderCurrency) {
+        OrderCurrency = orderCurrency;
+    }
+
+    public int getCustNo() {
+        return CustNo;
+    }
+    public void setCustNo(int custNo) {
+        CustNo = custNo;
+    }
+
+    public double getAmountInUSD() {
+        return AmountInUSD;
+    }
+
+
+    public void setAmountInUSD(double amountInUSD) {
+        AmountInUSD = amountInUSD;
+    }
+
+    public double getOrderAmount() {
+        return OrderAmount;
+    }
+
+   
+    public void setOrderAmount(double orderAmount) {
+        OrderAmount = orderAmount;
     }
 }
+
--- src/com/highradius/servlets/DataLoadingServlet.java
+++ src/com/highradius/servlets/DataLoadingServlet.java
@@ -14,6 +14,6 @@ public class DataLoadingServlet {
     }
 
     public List<Invoice> getInvoice() throws IOException {
-        return invoiceDao.getInvoice();
+        return invoiceDao.getInvoices();
     }
 }
